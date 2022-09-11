466999aec7dfc664cd729918b5971bfda61c2fc1
rakennus <aleksibraun@web.de>
2022-09-11 18:53:25 +0200

    site

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,17 +5,35 @@ let ctx;
 
 let secondsPassed = 0;
 let oldTimeStamp = 0;
-let fps = 0;
+
+let margin = 100;
 
 const results = [
-    {mood: "Angry", total: 1499, shade: "#0a9627"},
-    {mood: "Happy", total: 478, shade: "#960A2C"},
-    {mood: "Melancholic", total:332, shade: "#332E2E"},
-    {mood: "Gloomy", total: 195, shade: "#F73809"}
+    { title: "Russisch", value: 18, shade: "blue" },
+    { title: "Italienisch", value: 11, shade: "green" },
+    { title: "Türkisch", value: 11, shade: "red" },
+    { title: "Spanisch", value: 6, shade: "yellow" },
+    { title: "Französisch", value: 5, shade: "blue" },
+    { title: "Chinesisch", value: 5, shade: "red" },
+    { title: "Portugiesisch", value: 5, shade: "green" },
+    { title: "Polnisch", value: 4, shade: "red" },
+    { title: "Griechisch", value: 4, shade: "blue" },
+    { title: "Kurdisch", value: 3, shade: "green" },
+    { title: "Ungarisch", value: 3, shade: "red" },
+    { title: "Schwedisch", value: 2, shade: "yellow" },
+    { title: "Persisch", value: 2, shade: "#0a9627" },
+    { title: "Farsi", value: 2, shade: "#0a9627" },
+    { title: "Marokanisch", value: 2, shade: "#0a9627" },
+    { title: "Arabisch", value: 2, shade: "#0a9627" },
+    { title: "Afghanisch", value: 1, shade: "#0a9627" },
+    { title: "Indonesisch", value: 1, shade: "#0a9627" },
+    { title: "Ukrainisch", value: 1, shade: "#0a9627" },
+    { title: "Serbisch", value: 1, shade: "#0a9627" },
+    { title: "Niederländisch", value: 1, shade: "#0a9627" },
 ];
 
 let sum = 0;
-let totalNumberOfPeople = results.reduce((sum, {total}) => sum + total, 0);
+let totalNumberOfObjects = results.reduce((sum, { value }) => sum + value, 0);
 let currentAngle = 0;
 let multiplier = 0;
 
@@ -32,24 +50,18 @@ function gameLoop(timeStamp) {
     if (secondsPassed > 0.1) secondsPassed = 0.1;
     oldTimeStamp = timeStamp;
 
-    // Calculate fps
-    fps = Math.round(1 / secondsPassed);
-
-    if (!document.hidden) {
-        // Update game objects in the loop
-        update();
-        // Perform the drawing operation
-        draw();
+    if (multiplier < 1) {
+        multiplier += secondsPassed;
     }
+    if (multiplier > 1) {
+        multiplier = 1;
+    }
+    
+    draw();
 
     // The loop function has reached it's end. Keep requesting new frames
-    window.requestAnimationFrame(gameLoop);
-}
-
-function update() {
-    multiplier += 1 * secondsPassed;
-    if(multiplier >= 1) {
-        multiplier = 1;
+    if (multiplier != 1) {
+        window.requestAnimationFrame(gameLoop);
     }
 }
 
@@ -63,18 +75,47 @@ function draw() {
     ctx.fillStyle = 'black';
     ctx.fillText("FPS: " + fps, 4, 14);
     */
-    currentAngle = -1/2*Math.PI;
+    currentAngle = -0.5 * Math.PI;
 
-    for (let moodValue of results) {
+    for (let object of results) {
         //calculating the angle the slice (portion) will take in the chart
-        let portionAngle = multiplier * (moodValue.total / totalNumberOfPeople) * 2 * Math.PI;
+        let portionAngle = multiplier * (object.value / totalNumberOfObjects) * 2 * Math.PI;
         //drawing an arc and a line to the center to differentiate the slice from the rest
         ctx.beginPath();
-        ctx.arc(canvas.width/2, canvas.height/2, canvas.height/2-20, currentAngle, currentAngle + portionAngle);
+        ctx.arc(canvas.width / 2, canvas.height / 2, canvas.height / 2 - margin, currentAngle, currentAngle + portionAngle);
         currentAngle += portionAngle;
-        ctx.lineTo(canvas.width/2, canvas.height/2);
+        ctx.lineTo(canvas.width / 2, canvas.height / 2);
         //filling the slices with the corresponding mood's color
-        ctx.fillStyle = moodValue.shade;
+        ctx.fillStyle = "#0b3464";
         ctx.fill();
+
+        var x = Math.cos(currentAngle - portionAngle / 2) * (canvas.height / 2 - margin) + canvas.width / 2;
+        var y = Math.sin(currentAngle - portionAngle / 2) * (canvas.height / 2 - margin) + canvas.height / 2;
+        ctx.fillStyle = "black";
+
+        let percentage = ~~(object.value / totalNumberOfObjects * 1000) / 10;
+
+        if (percentage < 2) {
+            ctx.textAlign = "left";
+            ctx.translate(x, y);
+            ctx.rotate(currentAngle - portionAngle / 2);
+            ctx.font = '12px serif';
+            ctx.fillText(object.title, 0, 0);
+            ctx.setTransform(1, 0, 0, 1, 0, 0);
+        } else {
+            if (currentAngle - portionAngle / 2 + 0.5 * Math.PI < Math.PI) {
+                ctx.textAlign = "left";
+            } else {
+                ctx.textAlign = "right";
+            }
+            ctx.font = '12px serif';
+            ctx.fillText(object.title, x, y)
+
+            var x = Math.cos(currentAngle - portionAngle / 2) * 0.5 * (canvas.height / 2 + 160 - margin) + canvas.width / 2;
+            var y = Math.sin(currentAngle - portionAngle / 2) * 0.5 * (canvas.height / 2 + 160 - margin) + canvas.height / 2;
+            ctx.textAlign = "center";
+            ctx.fillStyle = "white";
+            ctx.fillText(percentage + "%", x, y)
+        }
     }
 }
